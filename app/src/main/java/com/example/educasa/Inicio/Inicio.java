18f822fb83e8df1a4b7774de7406b96c28c63b09
whelming <mljasson@gmail.com>
2019-06-07 19:28:06 -0500

    Proyecto Base
    División de Modulos

---
+++ app/src/main/java/com/example/educasa/Inicio/Inicio.java
@@ -0,0 +1,15 @@
+package com.example.educasa.Inicio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.educasa.R;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
