6426e8401a58434456614979c6a6daaac2254a3d
Zruth <48073323+Zruth@users.noreply.github.com>
2019-07-31 22:02:01 -0500

    detalle de solicitud
    implementando el spiner y el time y calendar

--- app/src/main/java/com/example/educasa/Inicio/InicioFragments/InicioMapaFragmen.java
+++ app/src/main/java/com/example/educasa/Inicio/InicioFragments/InicioMapaFragmen.java
@@ -25,70 +25,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-public class InicioMapaFragmen extends Fragment {
+public class InicioMapaFragmen extends Fragment implements  OnMapReadyCallback{
 
-
-    public InicioMapaFragmen() {
-        // Required empty public constructor
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onMapReady(GoogleMap googleMap) {
 
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_mapa, container, false);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-                mMap.clear(); //clear old markers
-
-                CameraPosition googlePlex = CameraPosition.builder()
-                        .target(new LatLng(37.4219999,-122.0862462))
-                        .zoom(10)
-                        .bearing(0)
-                        .tilt(45)
-                        .build();
-
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
-
-               /* mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(37.4219999, -122.0862462))
-                        .title("Spider Man")
-                        .icon(bitmapDescriptorFromVector(R.drawable.pin)));
-
-                mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(37.4629101,-122.2449094))
-                        .title("Iron Man")
-                        .snippet("His Talent : Plenty of money"));
-
-                mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(37.3092293,-122.1136845))
-                        .title("Captain America"));*/
-            }
-        });
-
-
-        return rootView;
-    }
-
-    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
-        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
-        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
-        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        vectorDrawable.draw(canvas);
-        return BitmapDescriptorFactory.fromBitmap(bitmap);
-    }
-
-
 }
--- app/src/main/java/com/example/educasa/Inicio/InicioFragments/InicioSolicitarCurso.java
+++ app/src/main/java/com/example/educasa/Inicio/InicioFragments/InicioSolicitarCurso.java
@@ -1,17 +1,21 @@
 package com.example.educasa.Inicio.InicioFragments;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentManager;
+
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,8 +24,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.educasa.Inicio.Inicio;
@@ -37,23 +43,24 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
 import com.google.android.material.navigation.NavigationView;
 
-public class InicioSolicitarCurso extends Fragment
-        implements OnMapReadyCallback {
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.Executor;
+
+public class InicioSolicitarCurso extends Fragment {
     private String bundlerecibido;
     private Button btn_confirmarclase;
     private Button btn_cancelar;
-    private GoogleMap Map;
     private Spinner materia;
-    FusedLocationProviderClient mFusedLocationProviderClient;
-    Location mLastKnownLocation;
-    Boolean mLocationPermissionGranted;
-    static final int DEFAULT_ZOOM = 15;
-    static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-
+    private Button bfecha,bhora;
+    private TextView fecha, hora;
+    private int dia,mes,ano,minutos;
 
     public InicioSolicitarCurso() {
+
     }
 
 
@@ -64,15 +71,16 @@ public class InicioSolicitarCurso extends Fragment
         btn_confirmarclase = v.findViewById(R.id.btn_confirmar_solicitar_curso);
         btn_cancelar = v.findViewById(R.id.btn_cancelar_curso);
         materia = (Spinner) v.findViewById(R.id.inicio_solicitud_curso_spinner);
+        bfecha = v.findViewById(R.id.solicitar_curso_fecha);
+        bhora = v.findViewById(R.id.inicio_solicitar_curso_hora);
+        bfecha.setOnClickListener((View.OnClickListener) this);
+        bhora.setOnClickListener((View.OnClickListener) this);
+
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.Materia, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         materia.setAdapter(adapter);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this); //referencia para acceder a la aplicacion
 
 
         bundlerecibido = getArguments().getString("datosolicitarcurso");
@@ -101,101 +109,34 @@ public class InicioSolicitarCurso extends Fragment
         return v;
     }
 
-    @Override
-    public void onMapReady(final GoogleMap googleMap) {
-        Map = googleMap;
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void onClick (View v){
+        Toast.makeText(getActivity(), "holaa", Toast.LENGTH_SHORT).show();
+        if (v== fecha){
+            final Calendar c= Calendar.getInstance();
+            dia = c.get(Calendar.DAY_OF_MONTH);
+            mes = c.get(Calendar.MONTH);
+            ano= c.get (Calendar.YEAR);
+            Toast.makeText(getActivity(), "holaadatee", Toast.LENGTH_SHORT).show();
+            DatePickerDialog datePickerDialog = new DatePickerDialog
+                    (v.getContext(), new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    fecha.setText(dayOfMonth+"/" +(monthOfYear+1)+"/"+year);
 
-        LatLng sydney = new LatLng(-16.32, -71.5188325);
-
-        getLocationPermission();
-        updateLocationUI();
-        getDeviceLocation();
-
-        Map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                Map.addMarker(new MarkerOptions().position(latLng).title("Mark2"));
-            }
-        });//agregar un marcador
-
-        Map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-                Map.addMarker(new MarkerOptions().position(latLng).title("Mark3").icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons(BitmapFactory.decodeResource(getResources(), R.drawable.carro), 100, 100)))); //cambiar el icono de nuestro marcador
-            }
-        });
-
-    }
-
-    private void getLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)//pregunta si hay permiso para acceder a nuestra ubicacion
-                == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        } else {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},//de lo contrario tratamoss de acceder a nuestro permiso
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        mLocationPermissionGranted = false;
-        switch (requestCode){
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
                 }
-            }
-        }
-        updateLocationUI();
-    }
-    private void updateLocationUI() {
-        if (Map == null) {
-            return;
-        }
-        try {
-            if (mLocationPermissionGranted) {
-                Map.setMyLocationEnabled(true); //para mostrar mi ubicacion
-                Map.getUiSettings().setMyLocationButtonEnabled(true); //para poner el botoncito de obtener nuestra aplicacion  getUiSettings() modificar la parte grafica de nuestro mapa
-            } else {
-                Map.setMyLocationEnabled(false);
-                Map.getUiSettings().setMyLocationButtonEnabled(false);
-                mLastKnownLocation = null;
-                getLocationPermission();
-            }
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
-        }
-    }
-    private void getDeviceLocation() {
-        /*
-         * Get the best and most recent location of the device, which may be null in rare
-         * cases when a location is not available.
-         */
-        try {
-            if (mLocationPermissionGranted) {
-                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
-                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            // Set the map's camera position to the current location of the device.
-                            mLastKnownLocation = task.getResult();
-                            LatLng miubicacion=new LatLng(mLastKnownLocation.getLatitude(),mLastKnownLocation.getLongitude());
-                            Map.moveCamera(CameraUpdateFactory.newLatLngZoom(miubicacion,15));
-                            mMap.addMarker(new MarkerOptions().position(miubicacion).title("Marke"));
-                        } else {
+            },
+            dia,mes,ano);
+            datePickerDialog.show();
 
-                        }
-                    }
-                });
-            }
-        } catch(SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
+
+        }if (v== hora){
+            final Calendar c= Calendar.getInstance();
+            hora = c.get(Calendar.HOUR_OF_DAY);
+            minutos= c.get (Calendar.MINUTE);
+
         }
     }
+
+
 }
